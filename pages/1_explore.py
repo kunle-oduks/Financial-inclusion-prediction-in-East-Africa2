5f7e53856a091ceceebb72737c5975befebf3237
kunle-oduks <119300177+kunle-oduks@users.noreply.github.com>
2024-04-10 11:43:28 +0100

    Update 1_explore.py

--- pages/1_explore.py
+++ pages/1_explore.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 import matplotlib.pyplot as plt
-import seaborn as sns
 import plotly.express as px
 import warnings
 warnings.filterwarnings('ignore')
